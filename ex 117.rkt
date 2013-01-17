bf36967dd2a8138980af31139800be15593f2ffa
Andrew Tran <atran246@gmail.com>
2013-01-17 12:47:05 -0600

    Ex 117 Wrote Data Definitions

---
+++ ex 117.rkt
@@ -0,0 +1,12 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-abbr-reader.ss" "lang")((modname |ex 117|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ())))
+;Andrew Tran
+
+;Ex 117
+
+; A List-of-amounts is one of:
+; - empty
+; - (cons PositiveNumber List-of-amounts)
+; interp. a List-of-amounts represents some amounts of money
+
