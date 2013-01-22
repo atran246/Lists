c26aa937b9a776a09c4b569535167b28987facdf
Andrew Tran <atran246@gmail.com>
2013-01-22 12:51:33 -0600

    Ex 118 Data Definitions

---
+++ ex 118.rkt
@@ -0,0 +1,11 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-abbr-reader.ss" "lang")((modname |ex 118|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ())))
+; Andrew Tran, EX 118
+
+; A List-of-numbers is one of:
+; - empty
+; - (cons Number List-of-numbers)
+; interpret a List-of-Numbers represents some list of numbers
+
+
