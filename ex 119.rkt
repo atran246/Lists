c59cdd611c8841b303729971790b2f9735bb0c7f
Andrew Tran <atran246@gmail.com>
2013-01-23 18:30:36 -0600

    Ex 119 Data Definitions

---
+++ ex 119.rkt
@@ -0,0 +1,9 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-abbr-reader.ss" "lang")((modname |ex 119|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ())))
+; Andrew Tran Ex. 119
+
+; A List-of-Boolean is one of:
+; - empty
+; - (cons BooleanValue List-of-Boolean)
+; interp. a List-of-Boolean represents some list of true/false values
