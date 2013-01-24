079ac9930b6bcf91149023d71da7740f73d2f8d7
Andrew Tran <atran246@gmail.com>
2013-01-23 20:16:43 -0600

    Ex 121 Data Definitions

---
+++ ex 121.rkt
@@ -0,0 +1,10 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-abbr-reader.ss" "lang")((modname |ex 121|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ())))
+;Andrew Tran Ex 121
+(require 2htdp/image)
+; A List-of-Images is one of:
+; - empty
+; - (cons Images List-of-Images)
+; interp. a List-of-Images represents a list of images
+
