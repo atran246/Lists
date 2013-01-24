9779ce63d2d738573bb87a369c5e1015ab121be4
Andrew Tran <atran246@gmail.com>
2013-01-23 19:18:42 -0600

    Ex. 120 Data Definitions

---
+++ ex 120.rkt
@@ -0,0 +1,9 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-abbr-reader.ss" "lang")((modname |ex 120|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ())))
+; Andrew Tran Ex 120
+
+; A List-of-Strings is one of:
+; - empty
+; - (cons String List-of-strings)
+; interp. a List-of-strings represents a list of strings
